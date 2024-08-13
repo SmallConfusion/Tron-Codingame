859d0edceb6f28aef12e025cd1f5deb964e11446
SmallConfusion <smallconfusion@protonmail.com>
2024-08-13 18:37:45 -0500

    Added basic safety testing

--- main.go
+++ main.go
@@ -31,9 +31,9 @@ type board [width * height]int
 func (d direction) get_vector() vector {
 	switch d {
 	case up:
-		return vector{0, 1}
-	case down:
 		return vector{0, -1}
+	case down:
+		return vector{0, 1}
 	case left:
 		return vector{-1, 0}
 	case right:
@@ -61,12 +61,15 @@ func (v vector) get_index() int {
 }
 
 func (v vector) add(x vector) vector {
-	return vector{v.X + x.X, v.Y + v.X}
+	return vector{v.X + x.X, v.Y + x.Y}
 }
 
-func (v *vector) add_self(x vector) {
-	v.X += x.X
-	v.Y += x.Y
+func (v vector) add_direction(x direction) vector {
+	return v.add(x.get_vector())
+}
+
+func (v vector) multiply_scalar(x int) vector {
+	return vector{v.X * x, v.Y * x}
 }
 
 func (b *board) set(pos vector, value int) {
@@ -74,22 +77,24 @@ func (b *board) set(pos vector, value int) {
 }
 
 func (b board) get(pos vector) int {
+	if pos.X < 0 || pos.X >= width || pos.Y < 0 || pos.Y >= height {
+		return 5
+	}
+
 	index := pos.get_index()
 
-	if index > len(b) {
-		return 5
-	} else {
-		return b[index]
-	}
+	return b[index]
 }
 
 func (b board) is_safe(pos vector) bool {
-	return b.get(pos) == 0
+	safe := b.get(pos) == 0
+
+	return safe
 }
 
 func (b *board) update(input []vector) {
-	for i := 0; i < len(input); i++ {
-		b.set(input[i], i+1)
+	for i, vec := range input {
+		b.set(vec, i+1)
 	}
 }
 
@@ -104,15 +109,17 @@ func (b board) debug_print() {
 	}
 }
 
-var game = board{}
-var current_direction = left
+var (
+	game = board{}
+
+	current_pos       vector
+	current_direction = left
+)
 
 func main() {
 	for {
 		input := get_input()
 		game.update(input)
-
-		game.debug_print()
 
 		move()
 	}
@@ -128,12 +135,50 @@ func get_input() []vector {
 		var X0, Y0, X1, Y1 int
 		fmt.Scan(&X0, &Y0, &X1, &Y1)
 
+		// otherwise we might miss the end of opponent
+		input = append(input, vector{X0, Y0})
+
 		input = append(input, vector{X1, Y1})
+
+		if i == P {
+			current_pos = vector{X1, Y1}
+		}
 	}
 
 	return input
 }
 
 func move() {
+	if game.is_safe(current_pos.add_direction(current_direction)) {
+		current_direction.print_command()
+		return
+	}
 
+	var best_direction direction
+	var best_open_count int
+
+	for _, dir := range directions {
+		if game.is_safe(current_pos.add_direction(dir)) {
+			open_count := open_square_count(current_pos, dir)
+
+			fmt.Fprintf(os.Stderr, "Tested %d, %d open squares\n", dir.get_vector(), open_count)
+
+			if open_count > best_open_count {
+				best_direction = dir
+				best_open_count = open_count
+			}
+		}
+	}
+
+	current_direction = best_direction
+
+	current_direction.print_command()
+}
+
+func open_square_count(pos vector, dir direction) int {
+	for count := 0; ; count++ {
+		if !game.is_safe(pos.add(dir.get_vector().multiply_scalar(count + 1))) {
+			return count
+		}
+	}
 }
